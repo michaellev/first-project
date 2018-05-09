ee20eb8a0cfe17e6844b406cbcf9e997a4f1da36
Shahar Eisenberg <shahareis1@gmail.com>
2018-05-09 15:47:07 +0300

    fix: don't unconditionally disableAndHide(playerStopElm) on player pause

--- index.js
+++ index.js
@@ -91,7 +91,6 @@ navigator.mediaDevices.getUserMedia({audio: true})
       } else {
         enableAndShow(playerStopElm)
       }
-      disableAndHide(playerStopElm)
       showDownloadElm()
     })
     recorderRecordElm.addEventListener('click', () => { recorder.start() })
